861d94f13c9705166d6f515a1d251ad076df6b9a
Weilin Shi <934587911@qq.com>
2017-12-04 09:46:23 +0800

    Unwrap cli helpers

--- bin/cli.js
+++ bin/cli.js
@@ -1,19 +1,40 @@
 #!/usr/bin/env node
 'use strict';
-const meow = require('meow');
+const minimist = require('minimist');
+const pkg = require('../package.json');
 const pngToIco = require('../');
 
-const cli = meow(`
+const argv = minimist(process.argv.slice(2));
+
+process.title = pkg.name;
+
+if (argv.version || argv.v) {
+	showVersion();
+}
+
+if (argv.help || argv.h) {
+	showHelp();
+}
+
+if (argv._.length === 0 && process.stdin.isTTY) {
+	console.error('Please give me an png image of 256x256 pixels.');
+	process.exit(1);
+}
+
+pngToIco(argv._[0]).then(buf => process.stdout.write(buf));
+
+function showHelp() {
+	console.log(`
 	Usage:
 		$ png-to-ico input > output
 
 	Example:
 		$ png-to-ico electron.png > electron.ico
 `);
-
-if (cli.input.length === 0 && process.stdin.isTTY) {
-	console.error('Please give me an png image of 256x256 pixels.');
-	process.exit(1);
+	process.exit(0);
 }
 
-pngToIco(cli.input[0]).then(buf => process.stdout.write(buf));
+function showVersion() {
+	console.log('v' + pkg.version);
+	process.exit(0);
+}
